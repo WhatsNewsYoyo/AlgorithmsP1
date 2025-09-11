9fb742b99ce7411095c0d47e96a1888480c21c27
Victor <victorlaureanovega@gmail.com>
2025-09-11 12:36:22 -0600

    dp

--- main.cpp
+++ main.cpp
@@ -69,9 +69,6 @@ void manacher(const string &s, int &count, int &maxLength, int &start) {
     start = (center - maxLength) / 2;
 };
 
-void hash(){
-    
-}
 
 #include <bits/stdc++.h>
 
@@ -125,14 +122,16 @@ int main() {
 /*
 int main(){
     vector<string> files = {"mcode1.txt", "mcode2.txt", "mcode3.txt", "mcode4.txt", "mcode5.txt"};
-
-    for (string &fname : files) {
-        ifstream in(fname);
-
-        string s;
-        in >> s;
-
-        cout << s << endl;
+    
+    for (string &fname : files) {    
+        ifstream in(fname, ios::binary);
+        if (!in) {
+            cout << "Error: No se pudo abrir " << fname << endl;
+            continue;
+        }
+        string s((istreambuf_iterator<char>(in)),istreambuf_iterator<char>());
+        cout << "Contenido de " << fname << ":\n";
+        cout << s << "\n"; // imprime con saltos reales
     }
 }
 */
